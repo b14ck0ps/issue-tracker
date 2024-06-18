24e75274bb9929f5310ef06f8cfcf4f5608410c2
Azran <mr69751@gmail.com>
2024-06-18 19:45:11 +0600

    impl: pagination

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,16 @@
 import Pagination from "./Components/Pagination";
 import NavBar from "./NavBar";
 
-export default function Home() {
-  return <Pagination itemCount={30} pageSize={10} currentPage={2} />;
+export default function Home({
+  searchParams,
+}: {
+  searchParams: { page: string };
+}) {
+  return (
+    <Pagination
+      itemCount={30}
+      pageSize={10}
+      currentPage={parseInt(searchParams.page)}
+    />
+  );
 }
