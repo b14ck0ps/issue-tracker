262cc8ba1e33c9e442b3e24c3b3cbe7128db8751
Azran <mr69751@gmail.com>
2024-06-18 14:21:45 +0600

    feat: Improve DeleteIssueButton component with delete confirmation dialog and loading spinner

--- app/issues/[id]/DeleteIssueButton.tsx
+++ app/issues/[id]/DeleteIssueButton.tsx
@@ -1,4 +1,5 @@
 'use client'
+import { Spinner } from '@/app/Components'
 import { AlertDialog, Button, Flex } from '@radix-ui/themes'
 import axios from 'axios'
 import { useRouter } from 'next/navigation'
@@ -7,12 +8,15 @@ import { useState } from 'react'
 const DeleteIssueButton = ({ issueId }: { issueId: number }) => {
     const router = useRouter()
     const [error, seterror] = useState(false)
+    const [isDeleting, setisDeleting] = useState(false)
     const deleteIssue = async () => {
         try {
+            setisDeleting(true)
             await axios.delete(`/api/issues/${issueId}`)
             router.push('/issues')
             router.refresh()
         } catch (error) {
+            setisDeleting(false)
             seterror(true)
         }
     }
@@ -20,7 +24,10 @@ const DeleteIssueButton = ({ issueId }: { issueId: number }) => {
         <>
             <AlertDialog.Root>
                 <AlertDialog.Trigger>
-                    <Button color='red'>Delete Issue</Button>
+                    <Button color='red' disabled={isDeleting}>
+                        Delete Issue
+                        {isDeleting && <Spinner />}
+                    </Button>
                 </AlertDialog.Trigger>
                 <AlertDialog.Content>
                     <AlertDialog.Title>Are you sure?</AlertDialog.Title>
@@ -32,7 +39,9 @@ const DeleteIssueButton = ({ issueId }: { issueId: number }) => {
                             <Button value="soft" color='gray'>Cancel</Button>
                         </AlertDialog.Cancel>
                         <AlertDialog.Action>
-                            <Button value="hard" color='red' onClick={deleteIssue} >Delete</Button>
+                            <Button value="hard" color='red' onClick={deleteIssue}>
+                                Delete
+                            </Button>
                         </AlertDialog.Action>
                     </Flex>
                 </AlertDialog.Content>
