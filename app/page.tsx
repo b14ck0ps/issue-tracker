1924f295614de2e1d208614ca6fa891dfbca5eba
Azran <mr69751@gmail.com>
2024-06-18 20:45:40 +0600

    refactor: Update Home component to use lazy loading for images and improve layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,23 @@
 import prisma from "@/prisma/client";
 import IssueSummary from "./IssueSummary";
 import { IssueCharts } from "./IssueCharts";
+import { Flex, Grid } from "@radix-ui/themes";
+import LatestIssues from "./LatestIssues";
 
 export default async function Home() {
-  const open = await prisma.issue.count({ where: { status: "OPEN" } });
-  const inProgress = await prisma.issue.count({
-    where: { status: "IN_PROGRESS" },
-  });
-  const closed = await prisma.issue.count({ where: { status: "CLOSED" } });
+  const issueCounts = {
+    open: await prisma.issue.count({ where: { status: "OPEN" } }),
+    inProgress: await prisma.issue.count({ where: { status: "IN_PROGRESS" } }),
+    closed: await prisma.issue.count({ where: { status: "CLOSED" } }),
+  };
 
-  return <IssueCharts open={open} inProgress={inProgress} closed={closed} />;
+  return (
+    <Grid columns={{ initial: "1", md: "2" }} gap="5">
+      <Flex direction="column" gap="5">
+        <IssueSummary {...issueCounts} />
+        <IssueCharts {...issueCounts} />
+      </Flex>
+      <LatestIssues />
+    </Grid>
+  );
 }
