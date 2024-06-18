3a09da87017a37e3d9d48f3a979b4620f1c4fc39
Azran <mr69751@gmail.com>
2024-06-18 14:15:26 +0600

    feat: Add delete confirmation dialog to DeleteIssueButton component

--- app/issues/[id]/DeleteIssueButton.tsx
+++ app/issues/[id]/DeleteIssueButton.tsx
@@ -2,33 +2,56 @@
 import { AlertDialog, Button, Flex } from '@radix-ui/themes'
 import axios from 'axios'
 import { useRouter } from 'next/navigation'
+import { useState } from 'react'
 
 const DeleteIssueButton = ({ issueId }: { issueId: number }) => {
     const router = useRouter()
+    const [error, seterror] = useState(false)
+    const deleteIssue = async () => {
+        try {
+            await axios.delete(`/api/issues/${issueId}`)
+            router.push('/issues')
+            router.refresh()
+        } catch (error) {
+            seterror(true)
+        }
+    }
     return (
-        <AlertDialog.Root>
-            <AlertDialog.Trigger>
-                <Button color='red'>Delete Issue</Button>
-            </AlertDialog.Trigger>
-            <AlertDialog.Content>
-                <AlertDialog.Title>Are you sure?</AlertDialog.Title>
-                <AlertDialog.Description>
-                    This will permanently delete the issue.
-                </AlertDialog.Description>
-                <Flex mt="4" gap="3">
-                    <AlertDialog.Cancel>
-                        <Button value="soft" color='gray'>Cancel</Button>
-                    </AlertDialog.Cancel>
-                    <AlertDialog.Action>
-                        <Button value="hard" color='red' onClick={async () => {
-                            await axios.delete(`/api/issues/${issueId}`)
-                            router.push('/issues')
-                            router.refresh()
-                        }} >Delete</Button>
-                    </AlertDialog.Action>
-                </Flex>
-            </AlertDialog.Content>
-        </AlertDialog.Root>
+        <>
+            <AlertDialog.Root>
+                <AlertDialog.Trigger>
+                    <Button color='red'>Delete Issue</Button>
+                </AlertDialog.Trigger>
+                <AlertDialog.Content>
+                    <AlertDialog.Title>Are you sure?</AlertDialog.Title>
+                    <AlertDialog.Description>
+                        This will permanently delete the issue.
+                    </AlertDialog.Description>
+                    <Flex mt="4" gap="3">
+                        <AlertDialog.Cancel>
+                            <Button value="soft" color='gray'>Cancel</Button>
+                        </AlertDialog.Cancel>
+                        <AlertDialog.Action>
+                            <Button value="hard" color='red' onClick={deleteIssue} >Delete</Button>
+                        </AlertDialog.Action>
+                    </Flex>
+                </AlertDialog.Content>
+            </AlertDialog.Root>
+
+            <AlertDialog.Root open={error}>
+                <AlertDialog.Content>
+                    <AlertDialog.Title>ERROR</AlertDialog.Title>
+                    <AlertDialog.Description>
+                        There was an error deleting the issue.
+                    </AlertDialog.Description>
+                    <Flex mt="4" gap="3">
+                        <AlertDialog.Action>
+                            <Button color='gray' onClick={() => seterror(false)}>Close</Button>
+                        </AlertDialog.Action>
+                    </Flex>
+                </AlertDialog.Content>
+            </AlertDialog.Root>
+        </>
     )
 }
 
