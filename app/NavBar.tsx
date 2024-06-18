e3f9ffa75d53ff6db83e81239f5033963703512b
Azran <mr69751@gmail.com>
2024-06-18 15:08:47 +0600

    refactor: Update NavBar component to include user avatar and dropdown menu
    The NavBar component in NavBar.tsx has been updated to include the user avatar and a dropdown menu for authenticated users. This change enhances the user experience by providing a visual representation of the user and a convenient way to access user-related actions such as signing out.

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,5 +1,5 @@
 'use client'
-import { Box, Container, Flex } from '@radix-ui/themes';
+import { Avatar, Box, Container, DropdownMenu, Flex, Text } from '@radix-ui/themes';
 import classNames from 'classnames';
 import { useSession } from 'next-auth/react';
 import Link from 'next/link';
@@ -35,7 +35,23 @@ const NavBar = () => {
                     </Flex>
 
                     <Box>
-                        {status === 'authenticated' && (<Link href="/api/auth/signout">Sign Out</Link>)}
+                        {status === 'authenticated' && (
+                            <DropdownMenu.Root>
+                                <DropdownMenu.Trigger>
+                                    <Avatar src={session.user!.image!} fallback="?" size="2" radius='full' className='cursor-pointer' />
+                                </DropdownMenu.Trigger>
+                                <DropdownMenu.Content>
+                                    <DropdownMenu.Label>
+                                        <Text>
+                                            {session.user!.email!}
+                                        </Text>
+                                    </DropdownMenu.Label>
+                                    <DropdownMenu.Item>
+                                        <Link href="/api/auth/signout">Sign Out</Link>
+                                    </DropdownMenu.Item>
+                                </DropdownMenu.Content>
+                            </DropdownMenu.Root>
+                        )}
                         {status === 'unauthenticated' && (<Link href="/api/auth/signin">Sign In</Link>)}
                     </Box>
                 </Flex>
