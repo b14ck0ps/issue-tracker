3ee74a587e32b5df2cd4b93ea35fb77914fc2d45
Azran <mr69751@gmail.com>
2024-06-21 14:34:03 +0600

    fixed skeleton padding

--- app/issues/list/loading.tsx
+++ app/issues/list/loading.tsx
@@ -1,38 +1,50 @@
-import { Table } from '@radix-ui/themes'
-import Skeleton from 'react-loading-skeleton'
-import IssueActions from './IssueActions'
+import { Flex, Table } from "@radix-ui/themes";
+import Skeleton from "react-loading-skeleton";
+import IssueActions from "./IssueActions";
 
 const LoadinIssuesPage = () => {
+  const issues = [1, 2, 3, 4, 5];
 
-    const issues = [1, 2, 3, 4, 5]
+  return (
+    <>
+      <Flex direction="column" gap="3">
+        <IssueActions />
 
-    return (
-        <>
-            <IssueActions />
+        <Table.Root variant="surface">
+          <Table.Header>
+            <Table.Row>
+              <Table.ColumnHeaderCell>Issue</Table.ColumnHeaderCell>
+              <Table.ColumnHeaderCell className="hidden md:table-cell">
+                Status
+              </Table.ColumnHeaderCell>
+              <Table.ColumnHeaderCell className="hidden md:table-cell">
+                Created At
+              </Table.ColumnHeaderCell>
+            </Table.Row>
+          </Table.Header>
+          <Table.Body>
+            {issues.map((issue) => (
+              <Table.Row key={issue}>
+                <Table.Cell>
+                  <Skeleton />
+                  <div className="block md:hidden">
+                    {" "}
+                    <Skeleton />{" "}
+                  </div>
+                </Table.Cell>
+                <Table.Cell className="hidden md:table-cell">
+                  <Skeleton />
+                </Table.Cell>
+                <Table.Cell className="hidden md:table-cell">
+                  <Skeleton />
+                </Table.Cell>
+              </Table.Row>
+            ))}
+          </Table.Body>
+        </Table.Root>
+      </Flex>
+    </>
+  );
+};
 
-            <Table.Root variant='surface'>
-                <Table.Header>
-                    <Table.Row>
-                        <Table.ColumnHeaderCell>Issue</Table.ColumnHeaderCell>
-                        <Table.ColumnHeaderCell className='hidden md:table-cell'>Status</Table.ColumnHeaderCell>
-                        <Table.ColumnHeaderCell className='hidden md:table-cell'>Created At</Table.ColumnHeaderCell>
-                    </Table.Row>
-                </Table.Header>
-                <Table.Body>
-                    {issues.map((issue) => (
-                        <Table.Row key={issue}>
-                            <Table.Cell>
-                                <Skeleton />
-                                <div className='block md:hidden'> <Skeleton /> </div>
-                            </Table.Cell>
-                            <Table.Cell className='hidden md:table-cell'><Skeleton /></Table.Cell>
-                            <Table.Cell className='hidden md:table-cell'><Skeleton /></Table.Cell>
-                        </Table.Row>
-                    ))}
-                </Table.Body>
-            </Table.Root>
-        </>
-    )
-}
-
-export default LoadinIssuesPage
+export default LoadinIssuesPage;
